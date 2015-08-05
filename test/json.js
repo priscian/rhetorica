222fefbb624cab22b7d93ed36e4427c4e77f1d5e
priscian <priscian@gmail.com>
2015-08-05 02:19:11 -0400

    Removing some directories from repository.

--- test/json.js
+++
@@ -1,7 +0,0 @@
-var test =
-{
-  Anadiplosis: { windowSize: 2, extra: '2' },
-  Epizeuxis: { windowSize: 2 },
-  Polysyndeton: { windowSize: 1, extra: '2' },
-  Isocolon: { windowSize: 3, extra: '1' }
-}
